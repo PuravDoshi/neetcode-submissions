110bc14bf030738c0495248d4f6a2fa29dbd24b6
Purav Nirav Doshi <pndoshi07@gmail.com>
2026-08-24 18:02:55 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        n = len(s)
+        m = len(t)
+        if m != n:
+            return False
+        self.s_hashmap = {}
+        self.t_hashmap = {}
+        for i in range(n):
+            if s[i] not in self.s_hashmap:
+                self.s_hashmap[s[i]] = 1
+            else:
+                self.s_hashmap[s[i]] = self.s_hashmap[s[i]] + 1
+        for i in range(m):
+            if t[i] not in self.t_hashmap:
+                self.t_hashmap[t[i]] = 1
+            else:
+                self.t_hashmap[t[i]] = self.t_hashmap[t[i]] + 1
+        for i in self.s_hashmap:
+            if i not in self.t_hashmap:
+                return False
+            else:
+                if self.s_hashmap[i] != self.t_hashmap[i]:
+                    return False
+        return True
